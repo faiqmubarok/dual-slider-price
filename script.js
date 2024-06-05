bb5f201e4e26014ffce28128199a3eb67a03bdb2
Achmad Ricky Budianto <achmadricky@Achmads-MacBook-Pro-2.local>
2024-06-05 15:14:10 +0700

    fix update min and max input range on product change

--- script.js
+++ script.js
@@ -1,35 +1,3 @@
-document.getElementsByName("Tipe-Produk").forEach(element => {
-    element.addEventListener("change", handleProductTypeChange);
-});
-
-document.getElementsByName('Kost-Budget').forEach(element => {
-    element.addEventListener('change', handleBudgetChange);
-});
-
-function handleProductTypeChange() {
-    let typeBudgetKost = document.getElementById('Type-Budget-Kost');
-    let judulRange = document.getElementById('judul-range');
-
-    if (this.checked && this.id === "Kontrakan") {
-        typeBudgetKost.classList.add('hidden');
-        judulRange.innerHTML = 'Harga per Tahun';
-    } else {
-        typeBudgetKost.classList.remove('hidden');
-        handleBudgetChange();
-    }
-}
-
-function handleBudgetChange() {
-    let judulRange = document.getElementById('judul-range');
-    let isBulananChecked = document.getElementById('Bulanan').checked;
-
-    if (isBulananChecked) {
-        judulRange.innerHTML = 'Harga per Bulan';
-    } else {
-        judulRange.innerHTML = 'Harga per Tahun';
-    }
-}
-
 // Javascript Slider
 document.addEventListener("DOMContentLoaded", function () {
     let minRange = document.getElementById("min-range");
@@ -40,19 +8,9 @@ document.addEventListener("DOMContentLoaded", function () {
     let minThumb = document.getElementById("min-thumb");
     let maxThumb = document.getElementById("max-thumb");
 
-    let min = 0;
-    let max = 5000000;
+    let minPrice = 0;
+    let maxPrice = 5000000;
     let step = 100000;
-
-    minRange.min = min;
-    minRange.max = max;
-    minRange.step = step;
-    minRange.value = min;
-
-    maxRange.min = min;
-    maxRange.max = max;
-    maxRange.step = step;
-    maxRange.value = max;
 
     function formatRupiah(angka) {
         let reverse = angka.toString().split('').reverse().join('');
@@ -61,12 +19,62 @@ document.addEventListener("DOMContentLoaded", function () {
         return ribuan;
     }
 
+    function updateRangeLimits(min, max) {
+        minPrice = min;
+        maxPrice = max;
+        minRange.step = step;
+        maxRange.step = step;
+        minRange.min = minPrice;
+        maxRange.min = minPrice;
+        minRange.max = maxPrice;
+        maxRange.max = maxPrice;
+        minRange.value = minPrice;
+        maxRange.value = maxPrice;
+        updateInputs();
+        updateTrackAndThumbs();
+    }
+
+    document.getElementsByName("Tipe-Produk").forEach(element => {
+        element.addEventListener("change", handleProductTypeChange);
+    });
+    
+    document.getElementsByName('Kost-Budget').forEach(element => {
+        element.addEventListener('change', handleBudgetChange);
+    });
+    
+    function handleProductTypeChange() {
+        let typeBudgetKost = document.getElementById('Type-Budget-Kost');
+        let judulRange = document.getElementById('judul-range');
+    
+        if (this.checked && this.id === "Kontrakan") {
+            updateRangeLimits(0, 30000000);
+            typeBudgetKost.classList.add('hidden');
+            judulRange.innerHTML = 'Harga per Tahun';
+        } else {
+            typeBudgetKost.classList.remove('hidden');
+            handleBudgetChange();
+        }
+    }
+    
+    function handleBudgetChange() {
+        let judulRange = document.getElementById('judul-range');
+        let isBulananChecked = document.getElementById('Bulanan').checked;
+    
+        if (isBulananChecked) {
+            judulRange.innerHTML = 'Harga per Bulan';
+            updateRangeLimits(0, 5000000);
+        } else {
+            judulRange.innerHTML = 'Harga per Tahun';
+            updateRangeLimits(0, 20000000);
+        }
+    }
+
     function updateTrackAndThumbs() {
         const minVal = parseInt(minRange.value);
         const maxVal = parseInt(maxRange.value);
 
-        const minPercent = ((minVal - min) / (max - min)) * 100;
-        const maxPercent = ((maxVal - min) / (max - min)) * 100;
+        const minPercent = ((minVal - minPrice) / (maxPrice - minPrice)) * 100;
+        const maxPercent = ((maxVal - minPrice) / (maxPrice - minPrice)) * 100;
 
         track.style.left = minPercent + "%";
         track.style.right = 100 - maxPercent + "%";
@@ -78,6 +86,16 @@ document.addEventListener("DOMContentLoaded", function () {
         maxInput.value = formatRupiah(maxVal);
     }
 
+    function updateInputs() {
+        minInput.value = minRange.value;
+        maxInput.value = maxRange.value;
+    }
+
+    function updateRanges() {
+        minRange.value = minInput.value;
+        maxRange.value = maxInput.value;
+    }
+
     function syncMinRange() {
         const minVal = Math.min(
             parseInt(minInput.value.replace(/\./g, '')) || 0,
@@ -85,16 +103,18 @@ document.addEventListener("DOMContentLoaded", function () {
         );
         minRange.value = minVal;
         minInput.value = formatRupiah(minVal);
+        updateRanges();
         updateTrackAndThumbs();
     }
 
     function syncMaxRange() {
         const maxVal = Math.max(
-            parseInt(maxInput.value.replace(/\./g, '')) || max,
+            parseInt(maxInput.value.replace(/\./g, '')) || maxPrice,
             parseInt(minRange.value) + step,
         );
         maxRange.value = maxVal;
         maxInput.value = formatRupiah(maxVal);
+        updateRanges();
         updateTrackAndThumbs();
     }
 
@@ -105,6 +125,7 @@ document.addEventListener("DOMContentLoaded", function () {
         if (parseInt(maxRange.value) < parseInt(minRange.value) + step) {
             maxRange.value = parseInt(minRange.value) + step;
         }
+        updateInputs();
         updateTrackAndThumbs();
     }
 
@@ -124,7 +145,7 @@ document.addEventListener("DOMContentLoaded", function () {
         syncMaxRange();
     });
 
-    updateTrackAndThumbs();
+    updateRangeLimits(minPrice, maxPrice);
 });
 
 // Reset Form
